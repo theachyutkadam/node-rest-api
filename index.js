7a14e853cafb695966078a94d3be4d8fc8052e3b
ak <theachyutkadam@gmail.com>
2024-02-26 22:21:59 +0530

    Update index file and added auto refresh page package

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
 const express = require('express');
 const mysql = require('mysql2');
 
@@ -8,24 +13,72 @@ app.get("/",function(request,response){
   response.send("Hello World!")
 })
 
-// app.use(express.json());
+app.use(express.json());
 
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password',
-//   database: 'node_rest_api_development',
-// });
+const db = mysql.createConnection({
+  host: 'localhost:3606',
+  user: 'root',
+  password: 'password',
+  database: 'node_rest_api_development',
+});
 
-// db.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to MySQL:', err);
-//     return;
-//   }
-//   console.log('Connected to MySQL database');
-// });
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+// ---------------------------------
+// Get all users
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// Get a user by ID
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('SELECT * FROM users WHERE id = ?', [id], (err, results) => {
+    if (err) throw err;
+    res.json(results[0]);
+  });
+});
+
+// Create a new user
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  db.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err, result) => {
+    if (err) throw err;
+    res.json({ message: 'User added successfully', id: result.insertId });
+  });
+});
+
+// Update a user
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  db.query('UPDATE users SET name = ?, email = ? WHERE id = ?', [name, email, id], (err) => {
+    if (err) throw err;
+    res.json({ message: 'User updated successfully' });
+  });
+});
+
+// Delete a user
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.query('DELETE FROM users WHERE id = ?', [id], (err) => {
+    if (err) throw err;
+    res.json({ message: 'User deleted successfully' });
+  });
 });
